95427310409690d2e0edd4d0cf85c933fb001674
ldocao <ldocao@gmail.com>
2015-09-21 20:13:51 +0200

    initialize training dataset

--- make_data.py
+++ make_data.py
@@ -2,25 +2,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.datasets
 import sklearn.linear_model
-import ipdb
 import plotting
 
 
 
-    
-# Generate a dataset and plot it
-np.random.seed(0)
-X, y = sklearn.datasets.make_moons(200, noise=0.20)
-plt.figure()
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+class Points():
+	def __init__(coordinates, label):
+		self.coordinates = coordinates 
+		self.label = label
 
 
-# Train the logistic rgeression classifier
-clf = sklearn.linear_model.LogisticRegressionCV()
-clf.fit(X, y)
- 
-# Plot the decision boundary
-plotting.plot_decision_boundary(X, y, lambda x: clf.predict(x))
-plt.title("Logistic Regression")
-plt.show()
+
+def generate_data(n_points, noise=0.2):
+    """Generate a dataset for training
+
+    Parameters:
+    ----------
+    n_points: integer
+        number of points for training
+
+    noise: float
+        noise in data
+    """
+    np.random.seed(0)
+    X, y = sklearn.datasets.make_moons(n_points, noise=noise)
+    return Points(X,y)
+
+
+
+
+
+
+# plt.figure()
+# plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+
+
+# # Train the logistic regression classifier
+# clf = sklearn.linear_model.LogisticRegressionCV()
+# clf.fit(X, y)
+
+# # Plot the decision boundary
+# plotting.plot_decision_boundary(X, y, lambda x: clf.predict(x))
+# plt.title("Logistic Regression")
+# plt.show()
+
 
