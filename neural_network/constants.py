e9d6cc35d1f22422d98ea97c3676e1ce7a102bd7
ldocao <ldocao@gmail.com>
2015-10-12 16:52:52 +0200

    feat: add file idlookuptable

--- neural_network/constants.py
+++ neural_network/constants.py
@@ -1,6 +1,8 @@
-FTRAIN = '../datasets/training.csv'
-FTEST = '../datasets/test.csv'
+FTRAIN = '../../datasets/training.csv'
+FTEST = '../../datasets/test.csv'
+FID = "../../datasets/IdLookupTable.csv"
 
 SIZE_IMAGE = 96
 HALF_SIZE_IMAGE = SIZE_IMAGE/2
 GRAYSCALE = 255.
+MAX_EPOCHS = 10
