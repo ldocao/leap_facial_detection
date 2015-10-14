ee1347309be3e18be87630b0fd00787e85ceaa10
ldocao <ldocao@gmail.com>
2015-10-14 09:59:17 +0200

    refactor: put into function

--- neural_network/output.py
+++ neural_network/output.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import single_hidden_layer as nn
 import load_data
+from constants import FID, HALF_SIZE_IMAGE
 import ipdb
 
 
@@ -17,7 +18,9 @@ results = nn.predict(features_test)
 def prepare_for_output(results):
     """Convert numpy array to dataframe"""
 
-
+    ##convert to pixels
+    results2 = results*float(HALF_SIZE_IMAGE) + float(HALF_SIZE_IMAGE)
+    ipdb.set_trace()
     ## refactor data
     header = ["RowId", "ImageId", "FeatureName", "Location"]
     n_images = 1783
@@ -28,7 +31,7 @@ def prepare_for_output(results):
     ##nested loops are not efficient at all, but who cares
     for image in xrange(n_images):
         for feat in xrange(n_features):
-            print rowid / (n_images*n_features) * 100., "%"
+            print float(rowid) / (n_images*n_features) * 100., "%"
             df.loc[rowid,:] = [rowid, 
                                image+1,
                                features_name[feat],
@@ -44,14 +47,33 @@ def prepare_for_output(results):
 def filter_lookuptable(df):
     """Output only the desired features"""
 
+    df = df.set_index(["ImageId","FeatureName"])
+    df_lookup = df_lookup = pd.read_csv(FID, names=[u'RowId2', u'ImageId', u'FeatureName', u'Location2'],skiprows=1)
+    df_lookup = df_lookup.set_index(["ImageId","FeatureName"])
+    joined_df = pd.concat([df,df_lookup],axis=1)
+    joined_df.drop("Location2",1,inplace=True)
+    joined_df.dropna(inplace=True)
+    joined_df = joined_df.set_index("RowId2")
+    joined_df["RowId"] = joined_df.index
+    joined_df["RowId"] = joined_df["RowId"].astype(int)
+    return joined_df.sort()
+
+
+
+
+
+
 
 
 
 
 df = prepare_for_output(results)
+df = filter_lookuptable(df)
+
+
 
 ##write to csv
-idlookuptable = ""
+
 ##filter using idlookuptable
 outputfile = "kaggle.csv"
-df.to_csv(outputfile, header=header, index=False)
+df.to_csv(outputfile, header=["RowId","Location"], index=False)
